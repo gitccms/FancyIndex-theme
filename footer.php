636b523a63327bbd322a49f128666e58d2d43540
zhangyucheng1021 <zhangyucheng1021@gmail.com>
2021-01-27 19:25:14 +0800

    2021

---
+++ footer.php
@@ -0,0 +1,61 @@
+                                </div>
+                            </article>
+                        </section>
+                    </main>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<div id="dfoot" class="footer">
+    <!--    <div id="f1" class="footer">-->
+    <!--        <br>-->
+    <!--        <h1>&nbsp;联系我们</h1>-->
+    <!--        <a href="mailto:admin@auoc.top"><i class="fas fa-envelope" style="margin-left: 50px;">&nbsp;</i>admin@auoc.top</a>-->
+    <!--    </div>-->
+    <!--    <div id="f2" class="footer">-->
+    <br><br><br><br><br><br>
+
+    <span><a href="#top">回到顶部</a></span><br><br>
+    <a href="https://github.com/zhangyucheng1021/fancy-files"><i class="fab fa-github">项目开源代码</i></a> <br><br><br>
+    © YOCEN 2020-<?php echo date("Y") ?> All Rights Reserved.<br><br>
+    <a id="备" href="http://www.beian.miit.gov.cn/">鲁ICP备20031334号</a> <br><br>
+    <!--    </div>-->
+    <!--    <div id="f3" class="footer">-->
+    <!--        <br><br><br><br><br><br><br><br>-->
+    <!--&lt;!&ndash;        <img id="foot_logo" src="/media/logo.png">&ndash;&gt;-->
+    <!--    </div>-->
+
+</div>
+</div>
+<script type="text/javascript" color="0,104,183" opacity="1" zindex="-1" count="100" src="/index/js/line.js"></script>
+<script type="text/javascript">
+    var ip_elem = document.getElementById("ones_ip");
+    if (ip_elem.innerText.length > 25) {
+        ip_elem.style.fontSize = "20px";
+    }
+    if (!IsPC()) {
+        document.getElementById("tb_main").style.width = "100%";
+        document.getElementById("head").style.fontSize = "25px";
+        document.getElementById("head").style.fontWeight = "bolder";
+        document.getElementById("p1").style.display = "none";
+        document.getElementById("but1").style.height = "35px";
+        document.getElementById("but1").style.fontSize = "15px";
+        ip_elem.style.fontSize = "20px";
+    }
+    // for (var i=0;i<ip_elem.innerText.length;i++){
+    //     if(ip_elem.innerText[i] == ':')
+    //     {
+    //         ip_elem.style.fontSize="25px";
+    //         break;
+    //     }
+    // }
+</script>
+<canvas id="c_n4" width="1536" height="722"
+        style="position: fixed; top: 0px; left: 0px; z-index: -1; opacity: 1;"></canvas>
+</body>
+</html>
+
+
+
+
---
+++ header.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<!-- saved from url=(0014)about:internet -->
+<html lang="zh-CN">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <script type="text/javascript">
+    </script>
+    <title>YOCEN's space</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link href="/index/css/painting.css" rel="stylesheet">
+    <script src="/index/js/jquery.js" type="text/javascript"></script>
+    <script src="/index/js/painting.js" type="text/javascript"></script>
+    <link href="/module/font-awesome/5.11.2/css/fontawesome.css" rel="stylesheet">
+    <link href="/module/font-awesome/5.11.2/css/brands.css" rel="stylesheet">
+    <link href="/module/font-awesome/5.11.2/css/solid.css" rel="stylesheet">
+    <script type="text/javascript" src="/js/jquery-3.5.1.min.js"></script>
+
+    <script src="/fancy-files/new fancy/js/fancy.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="/fancy-files/new%20fancy/css/fancy.css">
+    <link rel="shortcut icon" href="/media/logo.ico">
+
+    <style type="text/css">
+        .heart {
+            width: 10px;
+            height: 10px;
+            position: fixed;
+            background: #f00;
+            transform: rotate(45deg);
+            -webkit-transform: rotate(45deg);
+            -moz-transform: rotate(45deg);
+        }
+
+        .heart:after, .heart:before {
+            content: '';
+            width: inherit;
+            height: inherit;
+            background: inherit;
+            border-radius: 50%;
+            -webkit-border-radius: 50%;
+            -moz-border-radius: 50%;
+            position: absolute;
+        }
+
+        .heart:after {
+            top: -5px;
+        }
+
+        .heart:before {
+            left: -5px;
+        }</style>
+    <style type="text/css">
+        .heart {
+            width: 10px;
+            height: 10px;
+            position: fixed;
+            background: #f00;
+            transform: rotate(45deg);
+            -webkit-transform: rotate(45deg);
+            -moz-transform: rotate(45deg);
+        }
+
+        .heart:after, .heart:before {
+            content: '';
+            width: inherit;
+            height: inherit;
+            background: inherit;
+            border-radius: 50%;
+            -webkit-border-radius: 50%;
+            -moz-border-radius: 50%;
+            position: absolute;
+        }
+
+        .heart:after {
+            top: -5px;
+        }
+
+        .heart:before {
+            left: -5px;
+        }</style>
+    <style id="bsSuggest">
+        .jhover {
+            background: #07d;
+            color: #fff
+        }</style>
+</head>
+<body onload="domain()">
+<div class="header">
+    <div id="ipv6_logo"><a href="https://ipv6.yocen.top">
+            <img style="width: 100%;"
+                 src="/media/img/World_IPv6_launch_banner.svg"
+                 alt='ipv6 ready' title='ipv6 ready' border='0'>
+            <!--                <img src='http://ipv6-test.com/button-ipv6-small.png' alt='ipv6 ready' title='ipv6 ready' border='0'>-->
+        </a>
+    </div>
+    <main class="cd-main-content">
+        <div class="container">
+            <div class="wrapper-content article">
+                <div class="ibox">
+                    <div class="ibox-content animated fadeIn">
+                        <main>
+                            <section class="article-content">
+                                <header class="page-header">
+                                    <div class="text-center">
+
+                                            <h1 class="padding-bot10" style="font-weight: bolder;font-family: 'Microsoft YaHei UI'">Welcome to YOCEN's space-NetDisk</h1>
+                                    </div>
+                                </header>
+                                <article class="article" id="article">
+                                    <div id="dbody">
+                                        <div> <!--style="background: #088acb;height: 100px;width: 100%;">-->
+                                            <!--        <div id="ptime" style="float: left;width: 25%;font-size: 20px;margin-left: 5%;margin-top: 25px;"></div>-->
+                                            <!--        <div id="head_annocement" align="center" style="color:#f7c872;float:left;width: 50%;margin-right: auto;margin-left: auto;">Hello World!</div>-->
+                                            <!--        <div style="width:25%;float: right;display: none"><input id="switch_dark" type="button" value="暗黑模式"-->
+                                            <!--                                          style="float:right;font-size:15px;color: white;background-color: #3D5054;color:white;width: 50%;height: 20px;border-radius: 8px;"-->
+                                            <!--                                          onclick="change_darkmodel()"></div>-->
+                                        </div>
+                                        <hr width="90%" color="#000000">
+<!--                                        <div id="head_title"><a href="/">YOCEN's space</a></div><br>-->
+                                        <h1 id="indextitle" style="color: #000000;font-family: 'Microsoft New Tai Lue'"  >Index of
